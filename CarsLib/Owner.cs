af21193a6a41b53a0f9a6e6cbd3d0f8bc364c32d
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-09 15:30:50 +1000

    create owner class

--- CarsLib/Owner.cs
+++ CarsLib/Owner.cs
@@ -4,6 +4,24 @@ namespace CarsLib
 {
     public class Owner
     {
-        
+        public string Name { get; set; }
+        public int LicenceNo { get; set; }
+
+        public Owner (string name, int licenceNo)
+        {
+            this.Name = name;
+            SetLicenceNo(licenceNo);
+        }
+        public void SetLicenceNo(int licenceNo)
+        {
+            if (licenceNo.ToString().Length != 10)
+            {
+                this.LicenceNo = 0;
+            }
+            else
+            {
+                this.LicenceNo = licenceNo;
+            }
+        }
     }
 }
